e44371f7dc5241c96e315ad2a8f93852f9c27f47
blcat <bl_cat@163.com>
2018-06-24 09:39:52 +0800

    format #show node and use js load

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 addLoadEvent(event);
+addLoadEvent(createPic);
 
 function event(){
 	if(!document.getElementById('images') || !document.getElementsByTagName('a'))
@@ -39,4 +40,26 @@ function addLoadEvent(func){
 			func();
 		}
 	}
+}
+
+function createPic(){
+	var show = document.createElement('div');
+	show.setAttribute('id', 'show');
+	var pic = document.createElement('img');
+	pic.setAttribute('id', 'pic');
+	pic.setAttribute('src', 'images/pic.png');
+	pic.setAttribute('alt', 'My Girl Friend');
+	var description = document.createElement('h3');
+	var text = document.createTextNode('Choose a girl who make your heart peng peng peng !')
+	description.setAttribute('id', 'description');
+    description.appendChild(text);
+
+    show.appendChild(pic);
+    show.appendChild(description);
+
+    var body = document.getElementsByTagName('body')[0];
+    console.log(body);
+    body.appendChild(show);
+
+
 }
